160eaf34335ac89a1e3a39d208f5284c2e83c43d
Medha <medha8pandey@gmail.com>
2020-11-28 13:25:15 +0530

    made modular

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
-let theme = localStorage.getItem("theme");
 
-
-if(theme == null){
-    setTheme("light");
-}else{
-    setTheme(theme);
+let result ;
+let model = {
+    theme:"light",
+    location:"default.css",
+    ques:"What I do?",
+    ans:"I am a software developer and I love both code and coffee."
 }
+async function getData(){
+    result = await fetch("fixtures.json").then(res => res.json())
+}
+
+
+let modeObj = localStorage.getItem("modeObj");
+
+if(modeObj.theme == undefined){
+    setTheme(model);
+}else{
+    setTheme(modeObj);
+}
+getData();
 
 let themeDots = document.getElementsByClassName("quirk-dot");
 
 for(var i=0;i<themeDots.length;i++){
    themeDots[i].addEventListener("click",function(){
        let mode = this.dataset.mode;
-       setTheme(mode);
+       for (var j = 0; j< result.length;j++){
+           if(result[j].theme == mode){
+               setTheme(result[j])
+           }
+       }
+       
    })
 }
 
 function setTheme(mode) {
 
-    if(mode === "light"){
-        document.getElementById("theme-style").href="default.css";
-    }
-    else if(mode === "green"){
-        document.getElementById("theme-style").href="green.css";
-    }
-    else if(mode === "blue"){
-        document.getElementById("theme-style").href="blue.css";
-    }
-    else if(mode === "purple"){
-        document.getElementById("theme-style").href="purple.css";
-    }
+    document.getElementById("theme-style").href= mode.location;
+    document.getElementById("quirk-ques").innerText = mode.ques;
+    document.getElementById("quirk-ans").innerHTML = mode.ans;
 
-    localStorage.setItem("theme",mode);
+    localStorage.setItem("modeObj",mode);
     
 }
